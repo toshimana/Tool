95a4f5083d571909de2d614fc9fa1c120576d30b
toshi <toshi.mana.plus1@gmail.com>
2015-07-11 17:59:02 +0900

    Add Released Function.

--- Qt/Observed/include/ObservedPushButton.h
+++ Qt/Observed/include/ObservedPushButton.h
@@ -13,10 +13,12 @@ public:
 	ObservedPushButton( QWidget* parent = 0 );
 	virtual ~ObservedPushButton();
 
-	void connectPressed( std::function<void( void )> func );
+	void connectPressed ( std::function<void( void )> func );
+	void connectReleased( std::function<void( void )> func );
 
 private slots:
     void on_pressed( void );
+	void on_released( void );
 
 private:
 	struct Impl;
--- Qt/Observed/src/ObservedPushButton.cpp
+++ Qt/Observed/src/ObservedPushButton.cpp
@@ -5,13 +5,15 @@
 struct ObservedPushButton::Impl
 {
 	boost::signals2::signal<void( void )> pressed;
+	boost::signals2::signal<void( void )> released;
 };
 
 ObservedPushButton::ObservedPushButton( QWidget* parent )
 	: QPushButton( parent )
 	, mImpl( new ObservedPushButton::Impl() )
 {
-	connect( this, SIGNAL( pressed(void) ), this, SLOT( on_pressed(void) ) );
+	connect( this, SIGNAL( pressed ( void ) ), this, SLOT( on_pressed ( void ) ) );
+	connect( this, SIGNAL( released( void ) ), this, SLOT( on_released( void ) ) );
 }
 
 
@@ -29,4 +31,16 @@ void
 ObservedPushButton::on_pressed( void )
 {
 	mImpl->pressed();
+}
+
+void
+ObservedPushButton::connectReleased( std::function<void( void )> func )
+{
+	mImpl->released.connect( func );
+}
+
+void 
+ObservedPushButton::on_released( void )
+{
+	mImpl->released();
 }
