7be36935c0f441cb7281565f9de57058f2a52649
toshi <toshi.mana.plus1@gmail.com>
2015-07-11 17:57:58 +0900

    add IniParserHelper.

---
+++ IniParserHelper.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <boost/property_tree/ini_parser.hpp>
+
+class IniParserHelper
+{
+public:
+	IniParserHelper( const std::string& _section )
+		: section( _section )
+	{}
+
+	virtual ~IniParserHelper() {}
+
+	void read( const std::string& path )
+	{
+		boost::property_tree::ptree tree;
+		try {
+			boost::property_tree::read_ini( path, tree );
+		}
+		catch ( std::exception& e )
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		read( tree );
+	}
+
+	void read( const boost::property_tree::ptree& tree )
+	{
+		std::cout << '[' << section << ']' << std::endl;
+		read_impl( tree );
+	}
+
+	virtual void read_impl( const boost::property_tree::ptree& tree ) = 0;
+
+	const std::string section;
+};
