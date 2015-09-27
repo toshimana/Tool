350894c13f4ab88027b9ba4bc44624d1089882d6
toshi <toshiyuki_manabe@tctec.co.jp>
2015-09-27 17:21:00 +0900

    日付取得関数の追加

--- boosttool/posix_time.hpp
+++ boosttool/posix_time.hpp
@@ -6,11 +6,27 @@
 namespace boosttool
 {
 	std::string
-	getCurrentTimeStringMilliseconds( void )
+	getCurrentDateString( const boost::posix_time::ptime& ptime )
+	{
+		auto date = ptime.date();
+
+		return ( boost::format( "%04d%02d%02d" ) %
+			date.year() % static_cast<int>( date.month() ) % date.day()
+			).str();
+	}
+
+	std::string
+	getCurrentDateString( void )
 	{
 		namespace pt = boost::posix_time;
 
-		pt::ptime ptime = pt::microsec_clock::local_time();
+		pt::ptime ptime = pt::second_clock::local_time();
+		return getCurrentDateString( ptime );
+	}
+
+	std::string
+	getCurrentTimeStringMilliseconds( const boost::posix_time::ptime& ptime )
+	{
 		auto date = ptime.date();
 		auto time = ptime.time_of_day();
 
@@ -20,4 +36,14 @@ namespace boosttool
 			( time.fractional_seconds()/1000 )  // ミリ秒への変換：マイクロ秒 / 1000
 			).str();
 	}
+
+	std::string
+	getCurrentTimeStringMilliseconds( void )
+	{
+		namespace pt = boost::posix_time;
+
+		pt::ptime ptime = pt::microsec_clock::local_time();
+		return getCurrentTimeStringMilliseconds( ptime );
+	}
+
 };
