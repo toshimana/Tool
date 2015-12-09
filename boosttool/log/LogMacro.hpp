e703526660b33017466b89d16da9f3e35202fe59
toshi <toshiyuki_manabe@tctec.co.jp>
2015-12-09 15:26:04 +0900

    特定のエラーログ出力用のマクロ関数を定義

---
+++ boosttool/log/LogMacro.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <boost/log/trivial.hpp>
+#include <boost/format.hpp>
+
+#define ERROR_LOG( ... ) BOOST_LOG_TRIVIAL( error ) << ' ' << __FILE__ << '(' << __LINE__ << ')' << __FUNCTION__ << __VA_ARGS__
+
+#define ERROR_READ( file )  ERROR_LOG( ( boost::format( "fail to read : %s" ) % file ).str() )
+#define ERROR_PARSE( file ) ERROR_LOG( ( boost::format( "fail to parse : %s" ) % file ).str() )
