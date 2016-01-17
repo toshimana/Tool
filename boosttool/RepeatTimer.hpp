75fc9d339de418a87e38f289baba5c649bf7e93c
toshi <toshi.mana.plus1@gmail.com>
2016-01-17 23:08:42 +0900

    定周期処理を追加

---
+++ boosttool/RepeatTimer.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+#include <boost/thread.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/deadline_timer.hpp>
+
+namespace boosttool
+{
+	template <typename Func>
+	class RepeatTimer
+	{
+	public:
+		RepeatTimer( Func _func, const boost::posix_time::time_duration& _time )
+			: io()
+			, timer( io )
+			, func( _func )
+			, time( _time )
+		{
+			setting();
+			th.swap( boost::scoped_ptr<boost::thread>( new boost::thread( [this](){ io.run(); } ) ) );
+		}
+
+		~RepeatTimer( void )
+		{
+			io.stop();
+			th->join();
+		}
+
+		void repeater( void )
+		{
+			setting();
+			func();
+		}
+
+		void setting( void )
+		{
+			timer.expires_from_now( time );
+			timer.async_wait( boost::bind( &RepeatTimer::repeater, this ) );
+		}
+
+
+	private:
+		boost::asio::io_service io;
+		boost::asio::deadline_timer timer;
+		Func func;
+		const boost::posix_time::time_duration time;
+		boost::scoped_ptr<boost::thread> th;
+	};
+};
