94568c5df9b891fcba929ed38704ef6db27c8e57
toshi <toshiyuki_manabe@tctec.co.jp>
2016-01-20 04:48:34 +0900

    書き込みエラーログ出力を追加

--- boosttool/log/LogMacro.hpp
+++ boosttool/log/LogMacro.hpp
@@ -6,5 +6,6 @@
 #define ERROR_LOG( ... ) BOOST_LOG_TRIVIAL( error ) << ' ' << __FILE__ << '(' << __LINE__ << ')' << __FUNCTION__ << __VA_ARGS__
 
 #define ERROR_READ( file )  ERROR_LOG( ( boost::format( "fail to read : %s" ) % file ).str() )
+#define ERROR_WRITE( file ) ERROR_LOG( ( boost::format( "fail to write : %s" ) % file ).str() )
 #define ERROR_PARSE( file ) ERROR_LOG( ( boost::format( "fail to parse : %s" ) % file ).str() )
 #define ERROR_INITIALIZE( device ) ERROR_LOG( ( boost::format( "fail to initialize : %s" ) % device ).str() )
