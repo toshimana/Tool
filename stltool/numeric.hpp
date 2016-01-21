43777de0cde2c69a7c5fe9587b364f4404d7376f
toshi <toshiyuki_manabe@tctec.co.jp>
2016-01-21 19:05:05 +0900

    accumulateの関数使用版を追加

--- stltool/numeric.hpp
+++ stltool/numeric.hpp
@@ -10,4 +10,10 @@ namespace stltool
 	{
 		return std::accumulate( container.begin(), container.end(), init );
 	}
+
+	template <typename Container, class T, typename Func >
+	T accumulate( const Container& container, T init, Func func )
+	{
+		return std::accumulate( container.begin(), container.end(), init, func );
+	}
 };
